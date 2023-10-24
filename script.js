c76cfc0daa152df7a730f6505989d306b415819f
Raymond Zeng <shilei.zeng98@outlook.com>
2023-10-24 09:05:16 -0400

    Added HOF for handling error

--- script.js
+++ script.js
@@ -6,28 +6,31 @@ const input = document.querySelector("input");
 input.addEventListener('keypress', function(e){
   if(e.key == 'Enter'){
     query = document.querySelector("input").value;
-    requestGif(query)
+    safeRequestGif(query);
     document.querySelector("input").value = '';
   }
 })
 
 async function requestGif(query){
-  try{
-    const img = document.querySelector('img');
-    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}=${query}`, {mode: 'cors'});
-    
-    const gifJson = await response.json();
-    
-    img.src = gifJson.data.images.original.url;
-    errorMsg = document.querySelector(".error");
-    errorMsg.textContent = "";
-  } catch(error){
-    console.log(error)
-    errorMsg = document.querySelector(".error");
-    errorMsg.textContent = error;
-  }
-
+  const img = document.querySelector('img');
+  const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}=${query}`, {mode: 'cors'});
+  
+  const gifJson = await response.json();
+  
+  img.src = gifJson.data.images.original.url;
+  errorMsg = document.querySelector(".error");
+  errorMsg.textContent = "";
 }
 
-requestGif(query);
+function handleErrorRequestGif(fn){
+  return function(query){
+    return fn(query).catch(function(error){
+      console.log(error)
+    })
+  }
+}
+
+const safeRequestGif = handleErrorRequestGif(requestGif);
+
+safeRequestGif(query);
 
